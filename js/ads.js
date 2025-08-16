cb71c3b789fa44ccb626f2c143ae6f74941c0ca0
[Adebayo lekan] <[lekan5058@gmail.com]>
2025-08-16 22:05:23 +0100

    Initial commit

---
+++ js/ads.js
@@ -0,0 +1,4 @@
+var e=document.createElement('div');
+e.id='mQMvNskzZFoX';
+e.style.display='none';
+document.body.appendChild(e);
---
+++ text.banner.php
@@ -0,0 +1,52 @@
+<html xmlns="http://www.w3.org/1999/xhtml"><head></head><body>﻿
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<style type="text/css" title="FFN_button_banner">
+
+    #FFN_button_banner { margin:0;}
+
+    .btn { outline:none; float:left; margin:5px 2px 7px 2px; font-family:'Open Sans', Arial, Helvetica, sans-serif; font-size: 13px; text-decoration:none; text-align:center; }
+
+    .btn span { width:190px; height:16px; float:left; margin:-5px 0 0 0; padding:4px 0; border-left:1px solid #999999; border-right:1px solid #999999; }
+    .btn span span { position:relative; border-width:0; border-top:1px solid #999999; border-bottom:1px solid #999999; }
+
+    .btn span.label { color:#666; background-color:#ffffff; }
+	.btn span.label.active{color:#FFFFFF; background-color:#d31996; }
+
+    .btn:hover span { cursor:pointer; }
+    .btn:hover span.label{ color:#FFFFFF; background-color:#d31996; }
+
+    .btn:active span { background-position:0 -26px; }  /*Does not work in IE Trident*/
+   
+</style>
+
+
+<div id="FFN_button_banner" style="width: 186px; height: 232px;">
+<a onclick="gtag('event','Fuck Now',{'event_source_type':'USA_index'});" href="https://engine.partylemons.com/?447201987" target="_blank" title="Fuck Now" class="btn" rel="nofollow">
+	<span class="label active"><span>Fuck Now</span></span></a>
+<a onclick="gtag('event','Fuck tonight',{'event_source_type':'USA_index'});" href="https://engine.upsidedownrightside.com/?803963475" target="_blank" title="Fuck tonight" class="btn" rel="nofollow">
+	<span class="label"><span>Fuck tonight</span></span></a>
+<a onclick="gtag('event','HookUp tonight',{'event_source_type':'USA_index'});" href="https://engine.forbiddenhunter.com/?458218729" target="_blank" title="HookUp tonight" class="btn" rel="nofollow">
+	<span class="label"><span>HookUp tonight</span></span></a>
+<a onclick="gtag('event','Sugar babies',{'event_source_type':'USA_index'});" href="https://go.deegoodstuff.com/c7a649ea-b683-4e4b-9d67-9c977f2fb0b6" target="_blank" title="Sugar babies" class="btn" rel="nofollow">
+	<span class="label"><span>Sugar babies</span></span></a>
+<a onclick="gtag('event','Local Sex',{'event_source_type':'USA_index'});" href="https://engine.voluumtlkrnarketing.com/?468418794" target="_blank" title="Local Sex" class="btn" rel="nofollow">
+	<span class="label"><span>Local Sex</span></span></a>
+<a onclick="gtag('event','Milfs near you',{'event_source_type':'USA_index'});" href="https://one.pushtrk.info/c4d1916c-f2ee-4cde-ad76-7eb67030998c?" target="_blank" title="Milfs near you" class="btn" rel="nofollow">
+	<span class="label"><span>Milfs near you</span></span></a>
+<a onclick="gtag('event','Meet and fuck',{'event_source_type':'USA_index'});" href="https://one.pushtrk.info/15GWsh?" target="_blank" title="Meet and fuck" class="btn" rel="nofollow">
+	<span class="label"><span>Meet and fuck</span></span></a>
+</div>
+
+<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-F1PQBMEQW3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F1PQBMEQW3',{'send_page_view':false});
+</script>
+
+
+</body></html>
